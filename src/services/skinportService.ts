b6d476d6432fb182c65880c93f09a6cb66c2dc30
Alexander Silkin <mrmajestickk@gmail.com>
2025-01-11 18:40:04 -0500

    Figuring out tradable and non-tradable prices

--- src/services/skinportService.ts
+++ src/services/skinportService.ts
@@ -14,8 +14,6 @@ const fetchSkinportData = async(): Promise<any[]> => {
             throw new Error(`Skinport API returned status: ${response.status}`);
         }
         const data = await response.json();
-        console.log("FetchSkinportData");
-        console.log('Raw Skinport Data:', JSON.stringify(data, null, 2)); // Log the raw API response
         return data;
     } catch (error) {
         console.error('Error fetching Skinport data:', error);
@@ -24,9 +22,11 @@ const fetchSkinportData = async(): Promise<any[]> => {
 };
 
 const processSkinportData = (items: any[]): any[] => {
-    console.log(items)
+    // if min_price is not null than the item is tradable ?
     return items.map((item) => ({
-        name: item.market_hash_name
+        name: item.market_hash_name,
+        tradable_min_price: item.min_price ?? item.suggested_price,
+        non_tradable_min_price:item.min_price ?? item.suggested_price
     }));
 };
 
@@ -34,13 +34,19 @@ export const getSkinportItems = async (): Promise<any[]> => {
     const cacheKey = 'skinport';
     const cachedData = await redis_client.get(cacheKey);
 
+    //disable to avoid reading from redis
     if (cachedData){
         console.log('Cache hit');
         return JSON.parse(cachedData);
     }
+
     const rawData = await fetchSkinportData();
     const processedData = processSkinportData(rawData);
-
-    await redis_client.set(cacheKey, JSON.stringify(processedData));
+    
+    // save 
+    await redis_client.set(cacheKey, JSON.stringify(processedData),{
+        EX:300,
+        NX: true
+    });
     return processedData;
 }
